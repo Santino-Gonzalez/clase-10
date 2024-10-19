de550ca69147658f00fdfdda177fe8bd2e47cb27
Santino-Gonzalez <96146946+Santino-Gonzalez@users.noreply.github.com>
2024-10-19 14:53:25 -0300

    Escribe pasos
    Se escribieron los pasos principales del juego.

---
+++ main.js
@@ -0,0 +1,15 @@
+//FUNCION GENERAR Y MOSTRAR SECUENCIA
+
+//FUNCION TURNO DEL JUGADOR
+
+//FUNCION COMPROBAR SECUENCIA
+
+//INICIA EL JUEGO
+document.querySelector("#empezar").onclick = () => {
+    
+}
+
+//RESETEA EL JUEGO
+document.querySelector("#resetear").onclick = () => {
+
+}
