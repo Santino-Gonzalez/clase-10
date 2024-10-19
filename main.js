8019bbffc01dc4c77243a87fcba7e282c1d2c0f1
Santino-Gonzalez <96146946+Santino-Gonzalez@users.noreply.github.com>
2024-10-19 15:11:03 -0300

    Genera secuencia
    Se escribe la función para generar la secuencia que debe seguir el usuario.

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
+//VARIABLES
+let secuencia = [];
+const colores = document.querySelectorAll(".col");
+
 //FUNCION GENERAR Y MOSTRAR SECUENCIA
+
+function generarMostrarSecuencia(){
+    const color = Math.floor(Math.random() * colores.length);
+    secuencia.push(colores[color]);
+}
 
 //FUNCION TURNO DEL JUGADOR
 
