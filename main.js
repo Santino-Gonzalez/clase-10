1f38d062a3ea1d536ee0bd9baa491f6ddea47be6
Santino-Gonzalez <96146946+Santino-Gonzalez@users.noreply.github.com>
2024-10-25 23:12:03 -0300

    Muestra secuencia
    Ahora la función para generar la secuencia también la resalta para mostrarla

--- main.js
+++ main.js
@@ -4,9 +4,20 @@ const colores = document.querySelectorAll(".col");
 
 //FUNCION GENERAR Y MOSTRAR SECUENCIA
 
-function generarMostrarSecuencia(){
+function generarMostrarSecuencia() {
     const color = Math.floor(Math.random() * colores.length);
     secuencia.push(colores[color]);
+
+    secuencia.forEach((color, i) => {
+        let delay = (i + 1) * 500;
+
+        setTimeout(() => {
+            color.style.opacity = "100%";
+            setTimeout(() => {
+                color.style.opacity = "25%";
+            }, 250);
+        }, delay);
+    });
 }
 
 //FUNCION TURNO DEL JUGADOR
@@ -15,7 +26,7 @@ function generarMostrarSecuencia(){
 
 //INICIA EL JUEGO
 document.querySelector("#empezar").onclick = () => {
-    
+
 }
 
 //RESETEA EL JUEGO
