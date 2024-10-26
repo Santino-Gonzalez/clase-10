d50ede3e79484ff81b03bdaf04d9a455f0667331
Santino-Gonzalez <96146946+Santino-Gonzalez@users.noreply.github.com>
2024-10-25 23:47:55 -0300

    Agrega sonidos y secuencia de usuario
    Se agregan sonidos al resaltar los colores y la función que permite al usuario seguir la secuencia y que verifica si es la secuencia correcta.

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
 //VARIABLES
 let secuencia = [];
+let secuenciaUsuario = [];
+
 const colores = document.querySelectorAll(".col");
+const sonido1 = new Audio("sonidos/1.mp3");
+const sonido2 = new Audio("sonidos/2.mp3");
+const sonido3 = new Audio("sonidos/3.mp3");
+const sonido4 = new Audio("sonidos/4.mp3");
+
+//FUNCION RESALTAR
+
+function resaltarColor(color) {
+    color.style.opacity = "100%";
+    if (color.id === "verde") {
+        sonido1.play();
+    }
+    if (color.id === "rojo") {
+        sonido2.play();
+    }
+    if (color.id === "amarillo") {
+        sonido3.play();
+    }
+    if (color.id === "azul") {
+        sonido4.play();
+    }
+    setTimeout(() => {
+        color.style.opacity = "25%";
+    }, 250);
+}
 
 //FUNCION GENERAR Y MOSTRAR SECUENCIA
 
 function generarMostrarSecuencia() {
+    secuenciaUsuario = [];
     const color = Math.floor(Math.random() * colores.length);
     secuencia.push(colores[color]);
 
@@ -12,15 +40,28 @@ function generarMostrarSecuencia() {
         let delay = (i + 1) * 500;
 
         setTimeout(() => {
-            color.style.opacity = "100%";
-            setTimeout(() => {
-                color.style.opacity = "25%";
-            }, 250);
+            resaltarColor(color);
         }, delay);
     });
 }
 
 //FUNCION TURNO DEL JUGADOR
+
+function verificarSecuencia() {
+    colores.forEach(color => {
+        color.onclick = (e) => {
+            let colorUsuario = e.target;
+            resaltarColor(colorUsuario);
+            
+            secuenciaUsuario.push(e.target);
+            let colorSecuencia = secuencia[secuenciaUsuario.length - 1];
+
+            if(e.target != colorSecuencia){
+                alert("¡Error! La secuencia es incorrecta.");
+            }
+        }
+    });
+}
 
 //FUNCION COMPROBAR SECUENCIA
 
